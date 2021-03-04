2a825551438e923e9fa8a4962c442b8aeeab4183
Ali Phan <ali.os@code88.asia>
2021-03-04 15:21:31 +0700

    Init project

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# mongodb-demo
-Demo project for mongodb 
+# spring-boot-mongodb
+This repository contains a Spring Boot example project for MongoDB.
+
+For a code review of this repo, see my related [blog post](https://springframework.guru/3402-2/).
+
+You can learn more about my courses [here](http://courses.springframework.guru/courses/) on my site.
---
+++ src/main/java/guru/springframework/config/MongoConfig.java
@@ -0,0 +1,25 @@
+package guru.springframework.config;
+
+import java.io.IOException;
+import cz.jirutka.spring.embedmongo.EmbeddedMongoFactoryBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.core.*;
+import com.mongodb.MongoClient;
+
+
+@Configuration
+public class MongoConfig {
+
+    private static final String MONGO_DB_URL = "localhost";
+    private static final String MONGO_DB_NAME = "translate_db";
+    @Bean
+    public MongoTemplate mongoTemplate() throws IOException {
+        EmbeddedMongoFactoryBean mongo = new EmbeddedMongoFactoryBean();
+        mongo.setBindIp(MONGO_DB_URL);
+        mongo.setPort(55404);
+        MongoClient mongoClient = mongo.getObject();
+        MongoTemplate mongoTemplate = new MongoTemplate(mongoClient, MONGO_DB_NAME);
+        return mongoTemplate;
+    }
+}
